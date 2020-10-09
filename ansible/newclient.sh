757edda5346776f969dc8934036ea974c0770d31
Alan Oltrabella <alanoltrabella@gmail.com>
2020-10-09 17:48:17 +0200

    New client script and playbook

---
+++ ansible/newclient.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Ask for new client information.
+echo Generating new client:
+echo Insert client ID:
+read id_client
+echo Insert client email:
+read email_client
+echo Insert client name:
+read name_client
+echo Insert client surname:
+read surname_client
+echo "<customer>
+<id>$id_client</id>
+<email>$email_client</email>
+<name>$name_client</name>
+<surname>$surname_client</surname>
+</customer>" > new.client
+
+ansible-playbook newclient.yml
